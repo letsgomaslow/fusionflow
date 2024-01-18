79ea241feed4e696ae388e8046242f95067232ec
Ramanprimotech <ramandeep.singh@primotech.com>
2024-01-19 02:17:17 +0530

    corrected Maslow- Hyperlink

--- fusionflow-ChatEmbed/src/components/Badge.tsx
+++ fusionflow-ChatEmbed/src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://maslow.ai'}
+        href={'https://fusionflow.maslow.ai/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
