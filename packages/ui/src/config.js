bff5c64c19156b57f7171590749efcceea7a9c9e
Ramanprimotech <ramandeep.singh@primotech.com>
2024-02-15 02:57:38 +0530

    flowiseMerge_1.5.0

---
+++ packages/ui/src/config.js
@@ -0,0 +1,9 @@
+const config = {
+    // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
+    basename: '',
+    defaultPath: '/chatflows',
+    fontFamily: `'Roboto', sans-serif`,
+    borderRadius: 3
+}
+
+export default config
